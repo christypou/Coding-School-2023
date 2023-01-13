bacbe43151738788f7bd10bf3d733c97a75bbbf5
xtipou <xtipou@gmail.com>
2023-01-13 22:34:11 +0200

    Exercise Two

---
+++ Session-05/Session-05/ExerciseTwo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_05
+{
+    internal class ExerciseTwo
+    {
+        public int GetSum(int num)
+        {
+             int sum= 0;
+             for (int i = 1; i <= num; i++)
+             {
+                 sum += i;
+             }
+             
+            return sum;
+            
+        }
+        public int GetProduct(int num)
+        {
+             int product= 1;
+             for (int i = 1; i <= num; i++)
+             {
+                 product *=  i;
+             }
+             
+            return product;
+            
+        }
+    }
+}
---
+++ Session-05/Session-05/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Exercise Two - Ask for a number and the user chooses for sum or product
+using Session_05;
+
+ExerciseTwo exerciseTwo = new ExerciseTwo();
+Console.WriteLine("Give me a number:");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("If you want the sum press 1");
+Console.WriteLine("If you want the product press 2");
+int option = Convert.ToInt32(Console.ReadLine());
+
+while (option != 1 && option != 2)
+{
+    Console.WriteLine("Please give a valid option. ");
+    option = Convert.ToInt32(Console.ReadLine());
+}
+if (option == 1)
+{
+    Console.WriteLine("The sum is: "+exerciseTwo.GetSum(number));
+}
+else if (option == 2)
+{
+    Console.WriteLine("The product is: " + exerciseTwo.GetProduct(number));
+}
+
