ae4d5239ea4352466ef47379cd5c3b9a24af2105
xtipou <xtipou@gmail.com>
2023-01-16 14:52:27 +0200

    Added constructors in class University and some text in main

--- Session-06/Session-06/University.cs
+++ Session-06/Session-06/University.cs
@@ -13,6 +13,54 @@ namespace Session_06
         public Grade[] Grades { get; set; }
         public Schedule[] ScheduleCourse { get; set; }
 
+        public University() { }
+        public University(Student[] student)
+        {
+            Student = student;
+        }
+
+        public University(Student[] student, Courses[] course, Grade[] grades)
+        {
+            Student = student;
+            Course = course;
+            Grades = grades;
+        }
+        public University(Student[] student, Courses[] course, Grade[] grades, Schedule[] scheduleCourse)
+        {
+            Student = student;
+            Course = course;
+            Grades = grades;
+            ScheduleCourse = scheduleCourse;
+        }
+
+        public University(int id, string name, int yearsInService):base(id, name, yearsInService)
+        {
+
+        }
+        public University(int id, string name):base(id, name) { }
+        public University(int id):base(id) { }
+        public University(int id, string name, int yearsInService, Student[] student):base(id,name, yearsInService)
+        {
+            Student = student;   
+        }
+        public University(int id, string name, int yearsInService, Student[] student, Courses[] course) : base(id, name, yearsInService)
+        {
+            Student = student;
+            Course = course;
+        }
+        public University(int id, string name, int yearsInService, Student[] student, Courses[] course, Grade[] grades) : base(id, name, yearsInService)
+        {
+            Student = student;
+            Course = course;
+            Grades = grades;
+        }
+        public University(int id, string name, int yearsInService, Student[] student, Courses[] course, Grade[] grades, Schedule[] scheduleCourse) : base(id, name, yearsInService)
+        {
+            Student = student;
+            Course = course;
+            Grades = grades;
+            ScheduleCourse = scheduleCourse;
+        }
         public void GetStudents()
         {
 
