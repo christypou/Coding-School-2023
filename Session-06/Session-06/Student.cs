617f801e28d12d5ba68cc0b907ab1275f0f8dc53
xtipou <xtipou@gmail.com>
2023-01-16 14:28:09 +0200

    Student class : added constructors

--- Session-06/Session-06/Student.cs
+++ Session-06/Session-06/Student.cs
@@ -6,10 +6,36 @@ using System.Threading.Tasks;
 
 namespace Session_06
 {
-    internal class Student
+    internal class Student : Person
     {
         public int RegistrationNumber { get; set; }
         public Courses Courses { get; set; }
+
+        public Student() { }
+        public Student(int registrationNumber, Courses courses)
+        {
+            RegistrationNumber = registrationNumber;
+            Courses = courses;
+        }
+        public Student(int registrationNumber)
+        {
+            RegistrationNumber = registrationNumber;
+        }
+        
+        public Student(int id, string name): base(id,name) { 
+        }
+        public Student(int id,string name, int age): base(id,name,age) { 
+            
+        }
+        public Student(int id, string name, int age, int registrationNumber) : base(id, name, age)
+        {
+            RegistrationNumber = registrationNumber;
+        }
+        public Student(int id, string name, int age,int registrationNumber, Courses courses) : base(id, name, age)
+        {
+            RegistrationNumber = registrationNumber;
+            Courses= courses;
+        }
 
         public void Attend(Courses course, DateTime datetime)
         {
