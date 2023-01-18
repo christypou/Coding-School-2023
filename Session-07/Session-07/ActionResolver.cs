6518bf5cd036935bba3cf0ead84bb93142384aaf
xtipou <xtipou@gmail.com>
2023-01-18 15:58:09 +0200

    created methods for the actions

--- Session-07/Session-07/ActionResolver.cs
+++ Session-07/Session-07/ActionResolver.cs
@@ -20,86 +20,117 @@ namespace Session_07
             ActionResponse response = new ActionResponse();
             response.RequestId = request.RequestId;
             response.ResponseID = Guid.NewGuid();
-            //Message message = new Message();
-            //StringBuilder str=new StringBuilder();
-            switch (request.Action)
+            try
             {
-                case ActionEnum.Convert:
-                    decimal dec;
-                    int integer;
-                    if (Decimal.TryParse(request.Input, out dec))
-                    {
-                        string binary;
-                        int num = Convert.ToInt32(dec);
-                        binary = "";
-                        while (num > 1)
-                        {
-                            
-                            int remainder = num % 2;
-                            binary = Convert.ToString(remainder) + binary;
-                            num /= 2;
-                        }
-                        binary = Convert.ToString(num) + binary;
-                        //ActionResponse response1 = new ActionResponse(Guid.NewGuid(), binary, request.RequestId);
+                switch (request.Action)
+                {
+                    case ActionEnum.Convert:
+                        response.Output = Convert(request.Input);
+                        //decimal dec;
+                        //int integer;
+                        //if (Decimal.TryParse(request.Input, out dec))
+                        //{
+                        //    string binary;
+                        //    int num = Convert.ToInt32(dec);
+                        //    binary = "";
+                        //    while (num > 1)
+                        //    {
+
+                        //        int remainder = num % 2;
+                        //        binary = Convert.ToString(remainder) + binary;
+                        //        num /= 2;
+                        //    }
+                        //    binary = Convert.ToString(num) + binary;
                         //str.Append("The number is decimal and the binary is :" + binary);
                         //message.Messages=str.ToString();
                         return null;
-                    }
-                    else if(int.TryParse(request.Input, out integer))
-                    {
-                        string binary = Convert.ToString(integer);
-                        //ActionResponse response2 = new ActionResponse(Guid.NewGuid(), binary, request.RequestId);
-                        //str.Append("The number is integer and the binary is :" + binary);
+                        //}
+                        //else if(int.TryParse(request.Input, out integer))
+                        //{
+                        //    string binary = Convert.ToString(integer);
+                        //    //str.Append("The number is integer and the binary is :" + binary);
+                        //    //message.Messages = str.ToString();
+                        //    return null;
+                        //}
+                        //else
+                        //{
+
+                        //    return null;
+                        //    //str.Append("Wrong input!");
+                        //    //message.Messages = str.ToString();
+                        //}
+
+                        break;
+                    case ActionEnum.Uppercase:
+                        response.Output = Uppercase(request.Input);
+                        //int max = 0;
+                        //int wordnumber = 0;
+                        //string[] result = request.Input.Split(" ");
+                        //for (int i = 0; i < result.Length; i++)
+                        //{
+                        //    if (result[i].Length > max)
+                        //    {
+                        //        max = result[i].Length;
+                        //        wordnumber = i;
+                        //    }
+                        //}
+                        //string upperCaseWord = result[wordnumber];
+                        //str.Append("The new text is :" + upperCaseWord);
                         //message.Messages = str.ToString();
                         return null;
-                    }
-                    else
-                    {
-                      
-                        return null;
-                        //str.Append("Wrong input!");
+                        break;
+                    case ActionEnum.Reverse:
+                        response.Output = Reverse(request.Input);
+                        //char[] charArray = request.Input.ToCharArray();
+
+                        //string reversedString = String.Empty;
+
+                        //for (int i = charArray.Length - 1; i > -1; i--)
+                        //{
+                        //    reversedString += charArray[i];
+                        //}
+                        //str.Append("The reversed text is :" + reversedString);
                         //message.Messages = str.ToString();
-                    }
-                   
-                    break;
-                case ActionEnum.Uppercase:
-                    int max = 0;
-                    int wordnumber = 0;
-                    string[] result = request.Input.Split(" ");
-                    for (int i = 0; i < result.Length; i++)
-                    {
-                        if (result[i].Length > max)
-                        {
-                            max = result[i].Length;
-                            wordnumber = i;
-                        }
-                    }
-                    string upperCaseWord = result[wordnumber];
-                    //ActionResponse response3 = new ActionResponse(Guid.NewGuid(),upperCaseWord.ToUpper(), request.RequestId);
-                    //str.Append("The new text is :" + upperCaseWord);
-                    //message.Messages = str.ToString();
-                    return null;
-                    break;
-                case ActionEnum.Reverse:
-                    char[] charArray = request.Input.ToCharArray();
+                        return null;
+                        break;
+                    default:
+                        return null;
+                        break;
 
-                    string reversedString = String.Empty;
-
-                    for (int i = charArray.Length - 1; i > -1; i--)
-                    {
-                        reversedString += charArray[i];
-                    }
-                    //ActionResponse response4 = new ActionResponse(Guid.NewGuid(),reversedString, request.RequestId);
-                    //str.Append("The reversed text is :" + reversedString);
-                    //message.Messages = str.ToString();
-                    return null;
-                    break;
-                default:
-                    return null;
-                    break;
-                    
+                }
             }
-            
+            catch (Exception ex) { }
+            finally { }
+            return null;
+        }
+
+
+        public string Convert(string input)
+        {
+
+            StringConvert convert = new StringConvert();
+            convert.Text = input;
+
+            return convert.Manipulate();
+        }
+
+        public string Uppercase(string input)
+        {
+            // “Uppercase” you must check if the Input is a string and has more than
+            // one words(separated by a space), then find the longest word in the
+            // Input string and convert it to uppercase.
+
+
+
+            return input.ToUpper();
+        }
+
+        public string Reverse(string input)
+        {
+            // “Reverse” you must check if the Input is a string and reverse it.
+
+            return string.Empty;
+
         }
     }
 
---
+++ Session-07/Session-07/StringParser.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_07
+{
+    public class StringManipulator
+    {
+
+        public string Text { get; set; }
+
+        public virtual string Manipulate()
+        {
+
+            return string.Empty;
+        }
+
+    }
+
+    public class StringConvert : StringManipulator
+    {
+
+
+        public override string Manipulate()
+        {
+
+            // “Convert” you must check if the Input is a decimal number and convert it to binary.
+            // ...
+            //decimal dec;
+            //int integer;
+            //if (Decimal.TryParse(request.Input, out dec))
+            //{
+            //    string binary;
+            //    int num = Convert.ToInt32(dec);
+            //    binary = "";
+            //    while (num > 1)
+            //    {
+
+            //        int remainder = num % 2;
+            //        binary = Convert.ToString(remainder) + binary;
+            //        num /= 2;
+            //    }
+            //    binary = Convert.ToString(num) + binary;
+            //str.Append("The number is decimal and the binary is :" + binary);
+            //message.Messages=str.ToString();
+
+            //}
+            //else if(int.TryParse(request.Input, out integer))
+            //{
+            //    string binary = Convert.ToString(integer);
+            //    //str.Append("The number is integer and the binary is :" + binary);
+            //    //message.Messages = str.ToString();
+            //    return null;
+            //}
+            //else
+            //{
+
+            //    return null;
+            //    //str.Append("Wrong input!");
+            //    //message.Messages = str.ToString();
+            //}
+
+            return string.Empty;
+        }
+    }
+
+    public class StringReverse : StringManipulator
+    {
+
+        public override string Manipulate()
+        {
+            //char[] charArray = request.Input.ToCharArray();
+
+            //string reversedString = String.Empty;
+
+            //for (int i = charArray.Length - 1; i > -1; i--)
+            //{
+            //    reversedString += charArray[i];
+            //}
+            //str.Append("The reversed text is :" + reversedString);
+            //message.Messages = str.ToString();
+            return string.Empty;
+        }
+
+    }
+
+    public class StringUppercase : StringManipulator
+    {
+
+        public override string Manipulate()
+        {
+            //int max = 0;
+            //int wordnumber = 0;
+            //string[] result = request.Input.Split(" ");
+            //for (int i = 0; i < result.Length; i++)
+            //{
+            //    if (result[i].Length > max)
+            //    {
+            //        max = result[i].Length;
+            //        wordnumber = i;
+            //    }
+            //}
+            //string upperCaseWord = result[wordnumber];
+            //str.Append("The new text is :" + upperCaseWord);
+            //message.Messages = str.ToString();
+            return string.Empty;
+        }
+    }
+}
