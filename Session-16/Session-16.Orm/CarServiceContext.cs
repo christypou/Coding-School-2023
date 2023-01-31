40030af98ddfe55eff43af55092f443d89136890
xtipou <xtipou@gmail.com>
2023-01-31 14:07:49 +0200

    Created Car Service Context

--- Session-16/Session-16.Orm/CarServiceContext.cs
+++ Session-16/Session-16.Orm/CarServiceContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using LibCarService;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,27 @@ namespace Session_16.Orm
 {
     public class CarServiceContext : DbContext
     {
+        public DbSet<Car> Car { get; set; }
+        public DbSet<Customer> Customer { get; set; }
+        public DbSet<Engineer> Engineer { get; set; }
+        public DbSet<Manager> Manager { get; set; }
+        public DbSet<ServiceTask> ServiceTask { get; set; }
+        public DbSet<Transaction> Transaction { get; set; }
+        public DbSet<TransactionLine> transactionLine { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //change --------------------------------------------
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=TodoDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;");
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
 
     }
 }
