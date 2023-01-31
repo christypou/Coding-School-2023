2cf160101dff130e0ddb5ed676099e1b1897cf1a
xtipou <xtipou@gmail.com>
2023-01-31 14:01:00 +0200

    Copied project 11 and created all relations

---
+++ Session-16/LibCarService/Car.cs
@@ -0,0 +1,19 @@
+﻿namespace LibCarService 
+{
+    public class Car
+    {
+        public Guid ID { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string CarRegistrationNumber { get; set; }
+
+        public Car()
+        {
+            ID = Guid.NewGuid();
+        }
+
+        //Relations
+        public Transaction Transaction { get; set; }
+    }
+
+}
---
+++ Session-16/LibCarService/Engineer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibCarService 
+{
+    public class Engineer : Person
+    {
+        public Guid ManagerID { get; set; }
+        // Since the Expenses and the Total in the Monthly Ledger are decimal... It`s better to have decimal SalaryPerMonth
+        public decimal SalaryPerMonth { get; set; }
+        public Engineer() : base()
+        {
+            
+        }
+        //Relations
+        public Manager Manager { get; set; }
+        public List<TransactionLine> Lines { get; set; }
+    }
+}
