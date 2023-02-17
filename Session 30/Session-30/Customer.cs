5303295453f58053422b10588f16b9119c553a09
xtipou <xtipou@gmail.com>
2023-02-18 01:37:39 +0200

    Model

---
+++ Session 30/Session-30/Customer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Session_30
+{
+	public class Customer : Person
+	{
+		public Customer(string name, string surname, string cardnumber)
+		{
+			Name = name;
+			Surname = surname;
+			CardNumber = cardnumber;
+			
+			
+			Transactions = new List<Transaction>();
+
+	}
+		public string CardNumber { get; set; }
+
+
+	//Relations
+	public List<Transaction> Transactions { get; set; }
+}
+}
